197763af754ce0f4d7a770031cedc80c8c896cd8
Ondra Kandera <xergic@gmail.com>
2020-10-20 14:53:22 +0200

    Adds ability to completely disable menu

--- Source/FolioReaderConfig.swift
+++ Source/FolioReaderConfig.swift
@@ -162,6 +162,9 @@ open class FolioReaderConfig: NSObject {
     /// Go to saved position when open a book
     open var loadSavedPositionForCurrentBook = true
     
+    /// Disables menu
+    open var disableMenu = false
+    
     // MARK: Quote image share
 
     /// Custom Quote logo
